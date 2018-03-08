9f2ad2acb8a13f8aabfb403bffeaeac2a38280b1
rodigilber <rodigilber.gonzalez@gmail.com>
2018-03-08 17:28:10 -0600

    new menu and fixes

--- regimenes.php
+++ regimenes.php
@@ -19,7 +19,7 @@
 	$active_clientes="";
 	$active_usuarios="";	
 	$active_pagos   = "";
-	$title="Productos | ControlSys";
+	$title="Regimenes | ControlSys";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +37,7 @@
 	<div class="panel panel-info">
 		<div class="panel-heading">
 		    <div class="btn-group pull-right">
-				<button type='button' class="btn btn-info" data-toggle="modal" data-target="#nuevoProducto"><span class="glyphicon glyphicon-plus" ></span> Nuevo Regim&eacute;n</button>
+				<button type='button' class="btn btn-info" data-toggle="modal" data-target="#nuevoRegimen"><span class="glyphicon glyphicon-plus" ></span> Nuevo Regim&eacute;n</button>
 			</div>
 			<h4><i class='glyphicon glyphicon-search'></i> Buscar Regim&eacute;n</h4>
 		</div>
@@ -77,116 +77,116 @@
 
 <script>
 
-// Added By Jorge Manzano
-$(document).ready(function(){
+		// Added By Jorge Manzano
+		$(document).ready(function(){
 
-	// Carga los pagos realizados al recibo seleccionado
-	load(0);
+			// Carga los pagos realizados al recibo seleccionado
+			load(0);
 
-	// Activar el Menu
-	$(".catalogos").addClass("active");
+			// Activar el Menu
+			$(".catalogos").addClass("active");
 
-	// Al mostrar la ventana modal de pagos, posicionar el foco en la captura de la fecha
-	$('body').on("shown.bs.modal",".modal", function(){
+			// Al mostrar la ventana modal de pagos, posicionar el foco en la captura de la fecha
+			$('body').on("shown.bs.modal",".modal", function(){
 
-		    $(".modal-body input#codigo").focus();			
+				    $(".modal-body input#codigo").focus();			
 
-	});
+			});
 
-	// Resetear el contenido de la ventana modal de captura de pagos al cerrarla
-	$('body').on("hidden.bs.modal",".modal", function(){
-		
-		$(".alert").alert('close')
-	    $(".modal-body input#codigo").val("");
-	    $(".modal-body input#descripcion").val("");
+			// Resetear el contenido de la ventana modal de captura de pagos al cerrarla
+			$('body').on("hidden.bs.modal",".modal", function(){
+				
+				$(".alert").alert('close')
+			    $(".modal-body input#codigo").val("");
+			    $(".modal-body input#descripcion").val("");
 
-	});
+			});
 
-})
+		})
 
-// Carga la informacion de pagos de un recibo seleccionado
-function load(page){
-	$("#loader").fadeIn('slow');
+		// Carga la informacion de pagos de un recibo seleccionado
+		function load(page){
+			$("#loader").fadeIn('slow');
 
-	$.ajax({
-		url:'./ajax/buscar_regimen.php?action=ajax',
-			beforeSend: function(objeto){
-				$('#loader').html('<img src="./img/ajax-loader.gif"> Cargando...');
-			},
-			success:function(data){
-				$(".outer_div").html(data).fadeIn('slow');
-				$('#loader').html('');
-				$('[data-toggle="tooltip"]').tooltip({html:true});
-			}
-	})
-}
+			$.ajax({
+				url:'./ajax/buscar_regimen.php?action=ajax',
+					beforeSend: function(objeto){
+						$('#loader').html('<img src="./img/ajax-loader.gif"> Cargando...');
+					},
+					success:function(data){
+						$(".outer_div").html(data).fadeIn('slow');
+						$('#loader').html('');
+						$('[data-toggle="tooltip"]').tooltip({html:true});
+					}
+			})
+		}
 
-// Guardar Regimen
-$( "#guardar_regimen" ).submit(function( event ) {
+		// Guardar Regimen
+		$( "#guardar_regimen" ).submit(function( event ) {
 
-  		// $('#guardar_regimen').attr("disabled", true);
+		  		// $('#guardar_regimen').attr("disabled", true);
 
-  		// Se manda la informacion capturada a la base de datos
-		var parametros = $(this).serialize();
-		let hidden     = $("#idRegimen").val();
-		let action     = ( hidden != "0" ) ? "edit" : "save";
+		  		// Se manda la informacion capturada a la base de datos
+				var parametros = $(this).serialize();
+				let hidden     = $("#idRegimen").val();
+				let action     = ( hidden != "0" ) ? "edit" : "save";
 
-		$.ajax({
-			type: "POST",
-			url: "ajax/nuevo_regimen.php?action="+action,
-			data: parametros,
-			beforeSend: function(objeto){
-				$("#resultados_ajax_productos").html("Mensaje: Guardando...");
-			},
-			success: function(datos){
-				$("#resultados_ajax_productos").html(datos);
-				$(this).prop("disabled",true);
-				$("#idRegimen").val(0);
-				load(0);
-			}
-		});
+				$.ajax({
+					type: "POST",
+					url: "ajax/nuevo_regimen.php?action="+action,
+					data: parametros,
+					beforeSend: function(objeto){
+						$("#resultados_ajax_productos").html("Mensaje: Guardando...");
+					},
+					success: function(datos){
+						$("#resultados_ajax_productos").html(datos);
+						$(this).prop("disabled",true);
+						$("#idRegimen").val(0);
+						load(0);
+					}
+				});
 
-  	event.preventDefault();
-})
+		  	event.preventDefault();
+		})
 
-function editarRegimen(idRegimen)
-{
+		function editarRegimen(idRegimen)
+		{
 
-	$.ajax({
-		type: "POST",
-		url: "ajax/obtieneRegimen.php",
-		data: {idRegimen:idRegimen},
-		success: function(datos){
+			$.ajax({
+				type: "POST",
+				url: "ajax/obtieneRegimen.php",
+				data: {idRegimen:idRegimen},
+				success: function(datos){
 
-			// Abrir la ventana modal para modificacion delos regimenes
-			$('#nuevoProducto').modal();
+					// Abrir la ventana modal para modificacion delos regimenes
+					$('#nuevoRegimen').modal();
 
-			$("#codigo").val(datos.claveRegimen);
-			$("#descripcion").val(datos.regimen);
-			$("#idRegimen").val(datos.idRegimen);	
+					$("#codigo").val(datos.claveRegimen);
+					$("#descripcion").val(datos.regimen);
+					$("#idRegimen").val(datos.idRegimen);	
+
+				}
+			});
 
 		}
-	});
 
-}
+		// Eliminar Regimen
+		function eliminarRegimen(idRegimen)
+		{
+			if( confirm("Esta seguro de eliminar el regimen selecccionado?") )
+			{
+				$.ajax({
+					type: "POST",
+					url: "ajax/nuevo_regimen.php?action=delete",
+					data: {idRegimen:idRegimen},
+					success: function(datos){
+						alert("Regimen Eliminado exitosamente!");
+						load(0);
+					}
+				});
 
-// Eliminar Regimen
-function eliminarRegimen(idRegimen)
-{
-	if( confirm("Esta seguro de eliminar el regimen selecccionado?") )
-	{
-		$.ajax({
-			type: "POST",
-			url: "ajax/nuevo_regimen.php?action=delete",
-			data: {idRegimen:idRegimen},
-			success: function(datos){
-				alert("Regimen Eliminado exitosamente!");
-				load(0);
 			}
-		});
 
-	}
-
-}
+		}
 
 </script>
