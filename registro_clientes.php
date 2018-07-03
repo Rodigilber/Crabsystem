b3b9a81c6c67d4c9a41dc09d4809366f7716eda3
rodigilber <rodigilber.gonzalez@gmail.com>
2018-07-03 01:17:57 -0500

    no required

--- registro_clientes.php
+++ registro_clientes.php
@@ -8,7 +8,7 @@
 		<div class="modal-content">
 		  <div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nuevo cliente</h4>
+			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i>Agregar nuevo cliente</h4>
 		  </div>
 		  <div class="modal-body">
 			<form class="form-horizontal" method="post" id="guardar_cliente" name="guardar_cliente">
@@ -16,7 +16,7 @@
 
 
 				<div class="form-group">
-				<label for="NCONTRIBUYENTE" class="col-sm-3 control-label"># DE CONTRIBUYENTES</label>
+				<label for="NCONTRIBUYENTE" class="col-sm-3 control-label"># DE CONTRIBUYENTE</label>
 				<div class="col-sm-8">
 				  <input type="text" class="form-control" id="NCONTRIBUYENTE" name="NCONTRIBUYENTE">
 				</div>
@@ -25,21 +25,21 @@
 			  <div class="form-group">
 				<label for="nombre" class="col-sm-3 control-label">Nombre</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="nombre" name="nombre" required>
+				  <input type="text" class="form-control" id="nombre" name="nombre" >
 				</div>
 			  </div>
 
 			  	<div class="form-group">
 				<label for="initservprof" class="col-sm-3 control-label">INICIO DE SERV. PROF.</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="datepicker1" name="initservprof" required>
+				  <input type="text" class="form-control" id="datepicker1" name="initservprof" >
 				</div>
 			  </div>
 
 			  <div class="form-group">
 				<label for="finservprof" class="col-sm-3 control-label">FIN DE SERV. PROF.</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="datepicker2" name="finservprof" required>
+				  <input type="text" class="form-control" id="datepicker2" name="finservprof" >
 				</div>
 			  </div>
 
@@ -47,7 +47,7 @@
 				<div class="form-group">
 				<label for="regfiscal" class="col-sm-3 control-label">RÉGIMEN FISCAL </label>
 				<div class="col-sm-8">
-				 <select class="form-control" id="regfiscal" name="regfiscal" required>
+				 <select class="form-control" id="regfiscal" name="regfiscal" >
 					<option value="" selected>-- Selecciona un regimen --</option>
 					<option value="RIF" >RIF</option>
 					<option value="PF con Activ. Emp. y Prof.">PF con Activ. Emp. y Prof.</option>
@@ -75,7 +75,7 @@
 			  <div class="form-group">
 				<label for="contabilidad" class="col-sm-3 control-label">CONTABILIDAD </label>
 				<div class="col-sm-8">
-				 <select class="form-control" id="contabilidad" name="contabilidad" required>
+				 <select class="form-control" id="contabilidad" name="contabilidad" >
 					<option value="" selected>-- Selecciona una opcion --</option>
 					<option value="DESC. XML" >DESC. XML</option>
 					<option value="CONTPAQ">CONTPAQ</option>
@@ -87,7 +87,7 @@
 			  	<div class="form-group">
 				<label for="obfiscales" class="col-sm-3 control-label">OBLIGACIONES FISCALES </label>
 				<div class="col-sm-8">
-				 <select class="form-control" id="obfiscales" name="obfiscales" required>
+				 <select class="form-control" id="obfiscales" name="obfiscales" >
 					<option value="" selected>-- Selecciona una opcion --</option>
 					<option value="ISR" >ISR</option>
 					<option value="IVA">IVA</option>
@@ -123,35 +123,35 @@
 			  <div class="form-group">
 				<label for="auxiliar" class="col-sm-3 control-label">AUXILIAR</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="auxiliar" name="auxiliar" required>
+				  <input type="text" class="form-control" id="auxiliar" name="auxiliar" >
 				</div>
 			  </div>
 			  
 			  	<div class="form-group">
 				<label for="rfc" class="col-sm-3 control-label">RFC</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="rfc" name="rfc" required>
+				  <input type="text" class="form-control" id="rfc" name="rfc" >
 				</div>
 			  </div>
 			  
 			  	<div class="form-group">
 				<label for="contrdrfc" class="col-sm-3 control-label">CONTRASEÑA DEL RFC</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="contrdrfc" name="contrdrfc" required>
+				  <input type="text" class="form-control" id="contrdrfc" name="contrdrfc" >
 				</div>
 			  </div>
 			  
 			  	<div class="form-group">
 				<label for="contrefirma" class="col-sm-3 control-label">CONTRASEÑA E-FIRMA</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="contrefirma" name="contrefirma" required>
+				  <input type="text" class="form-control" id="contrefirma" name="contrefirma" >
 				</div>
 			  </div>
 
 			  		<div class="form-group">
 				<label for="expefirma" class="col-sm-3 control-label">VIGENCIA E-FIRMA</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="datepicker3" name="expefirma" required>
+				  <input type="text" class="form-control" id="datepicker3" name="expefirma" >
 				</div>
 			  </div>
      
@@ -159,7 +159,7 @@
 			  <div class="form-group">
 				<label for="nrp" class="col-sm-3 control-label">NRP</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="nrp" name="nrp" required>
+				  <input type="text" class="form-control" id="nrp" name="nrp" >
 				</div>
 			  </div>
 		
@@ -210,7 +210,7 @@
 			  <div class="form-group">
 				<label for="estado" class="col-sm-3 control-label">Estado</label>
 				<div class="col-sm-8">
-				 <select class="form-control" id="estado" name="estado" required>
+				 <select class="form-control" id="estado" name="estado" >
 					<option value="">-- Selecciona estado --</option>
 					<option value="1" selected>Activo</option>
 					<option value="0">Inactivo</option>
